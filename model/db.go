99ec8dc01405cdc7656123d887c2266c83438d9d
dandoh <dainhan605@gmail.com>
2017-07-22 16:42:29 +0900

    first commit

--- model/db.go
+++ model/db.go
@@ -13,6 +13,8 @@ const (
 
 var db *gorm.DB
 
+const dev = true
+
 func Init() {
 	var err error
 	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", DB_USER, DB_PASSWORD, DB_NAME)
@@ -22,53 +24,45 @@ func Init() {
 		panic("failed to connect database")
 	}
 
-	if !db.HasTable(&Group{}) {
-		db.CreateTable(&Group{})
+	if dev {
+		db.DropTable(&User{})
+		db.DropTable(&Group{})
+		db.DropTable(&Report{})
+		db.DropTable(&Comment{})
+		db.DropTable(&Todo{})
 	}
 
-	if !db.HasTable(&User{}) {
-		db.CreateTable(&User{})
-	}
-
-	if !db.HasTable(&Report{}) {
-		db.CreateTable(&Report{})
-	}
-
-	if !db.HasTable(&Todo{}) {
-		db.CreateTable(&Todo{})
-	}
-
-	if !db.HasTable(&Comment{}) {
-		db.CreateTable(&Comment{})
-	}
 	db.LogMode(true)
-
 	db.AutoMigrate(&User{})
 	db.AutoMigrate(&Group{})
 	db.AutoMigrate(&Report{})
 	db.AutoMigrate(&Comment{})
 	db.AutoMigrate(&Todo{})
 
-	user1 := User{Name:"Nhan", Email:"Dandoh@mgail.com", PasswordMD5:"haha"}
-	user2 := User{Name:"De", Email:"De@mgail.com", PasswordMD5:"haha"}
-	user3 := User{Name:"Shiki", Email:"Shiki@mgail.com", PasswordMD5:"haha"}
-	db.Create(&user1)
-	db.Create(&user2)
-	db.Create(&user3)
+	if dev {
 
-	group1 := Group{Name:"intern"}
-	group2 := Group{Name:"intern2"}
-	group3 := Group{Name:"intern3"}
-	db.Create(&group1)
-	db.Create(&group2)
-	db.Create(&group3)
+		user1 := User{Name: "Nhan", Email: "Dandoh@mgail.com", PasswordMD5: "haha"}
+		user2 := User{Name: "De", Email: "De@mgail.com", PasswordMD5: "haha"}
+		user3 := User{Name: "Shiki", Email: "Shiki@mgail.com", PasswordMD5: "haha"}
+		db.Create(&user1)
+		db.Create(&user2)
+		db.Create(&user3)
 
-	db.Model(&user1).Association("Groups").Append(group1)
-	db.Model(&user1).Association("Groups").Append(group3)
-	db.Model(&user1).Association("Groups").Append(group2)
-	db.Model(&user2).Association("Groups").Append(group3)
-	db.Model(&user3).Association("Groups").Append(group1)
-	db.Model(&user3).Association("Groups").Append(group2)
+		group1 := Group{Name: "intern"}
+		group2 := Group{Name: "intern2"}
+		group3 := Group{Name: "intern3"}
+		db.Create(&group1)
+		db.Create(&group2)
+		db.Create(&group3)
+
+		db.Model(&user1).Association("Groups").Append(group1)
+		db.Model(&user1).Association("Groups").Append(group3)
+		db.Model(&user1).Association("Groups").Append(group2)
+		db.Model(&user2).Association("Groups").Append(group3)
+		db.Model(&user3).Association("Groups").Append(group1)
+		db.Model(&user3).Association("Groups").Append(group2)
+
+	}
 
 }
 
@@ -77,9 +71,5 @@ func Get() *gorm.DB {
 }
 
 func Close() {
-	db.DropTable(&User{})
-	db.DropTable(&Group{})
-	db.DropTable(&Report{})
-	db.DropTable(&Comment{})
-	db.DropTable(&Todo{})
+	db.Close()
 }
