b817bbc6ac2c5c636204da9b40fb33ef6a90346b
dandoh <dainhan605@gmail.com>
2017-08-09 09:43:47 +0900

    split authorization

--- auth/auth.go
+++ auth/auth.go
@@ -1,1 +1,64 @@
 package auth
+
+import (
+	"regexp"
+	"github.com/dgrijalva/jwt-go"
+	"fmt"
+	"net/http"
+	"github.com/dandoh/sdr/util"
+	"github.com/dandoh/sdr/model"
+	"context"
+	"os"
+)
+
+type Claims struct {
+	UserID   uint   `json:"userId"`
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
+
+
+func RequireAuth(next http.Handler) http.Handler {
+	jwtSecret := []byte(os.Getenv("SECRET_KEY"));
+	fmt.Println("Here dude")
+	fmt.Println(jwtSecret)
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		util.PrintBody(r)
+		// extract jwt
+		authorizationHeader := r.Header.Get("Authorization")
+		authRegex, _ := regexp.Compile("(?:Bearer *)([^ ]+)(?: *)")
+		authRegexMatches := authRegex.FindStringSubmatch(authorizationHeader)
+		if len(authRegexMatches) != 2 {
+			// didn't match valid Authorization header pattern
+			http.Error(w, "not authorized", http.StatusUnauthorized)
+			return
+		}
+		jwtToken := authRegexMatches[1]
+
+		// parse tokentoken
+		token, err := jwt.ParseWithClaims(jwtToken, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("Unexpected signing method")
+			}
+			return jwtSecret, nil
+		})
+		if err != nil {
+			http.Error(w, "not authorized", http.StatusUnauthorized)
+			return
+		}
+
+		// extract claims
+		claims, ok := token.Claims.(*Claims)
+		if !ok || !token.Valid {
+			http.Error(w, "not authorized", http.StatusUnauthorized)
+			return
+		}
+		// load userID
+		authContext := model.AuthorContext{
+			AuthorID: claims.UserID,
+		}
+		ctx := context.WithValue(r.Context(), "authorContext", authContext)
+
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
--- auth/normal.go
+++ auth/normal.go
@@ -1,1 +1,112 @@
 package auth
+
+import (
+	"io/ioutil"
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"net/http"
+	"encoding/json"
+	"github.com/dandoh/sdr/util"
+	"github.com/dandoh/sdr/model"
+	"time"
+	"os"
+)
+
+type LoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type SignupRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Email    string    `json:"email"`
+}
+
+func confirmLogin(requestbody LoginRequest) (uint, bool) {
+	username := requestbody.Username
+	password := requestbody.Password
+	return model.GetUserID(username, password)
+}
+
+func confirmSignUp(requestbody SignupRequest) bool {
+	username := requestbody.Username
+	password := requestbody.Password
+	email := requestbody.Email
+	if (model.IsUserExisted(username, email) == false) {
+		var user model.User = model.User{Name: username, PasswordMD5: util.GetMD5Hash(password), Email: email}
+		model.CreateUser(&user)
+		return true
+	}
+	return false
+}
+
+func SignupFunc(w http.ResponseWriter, req *http.Request) {
+	// get username & password
+	bodyBytes, _ := ioutil.ReadAll(req.Body)
+	requestBody := SignupRequest{}
+	util.PrintBody(req)
+	err := json.Unmarshal(bodyBytes, &requestBody)
+	fmt.Printf("%+v", requestBody)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	success := confirmSignUp(requestBody)
+
+	var result string = "fail"
+	if (success) {
+		result = "success"
+	}
+
+	statusResponse := struct {
+		Status string `json:"status"`
+	}{Status: result}
+
+	json.NewEncoder(w).Encode(statusResponse)
+}
+
+func LoginFunc(w http.ResponseWriter, req *http.Request) {
+	// get username & password
+	jwtSecret := []byte(os.Getenv("SECRET_KEY"));
+	bodyBytes, _ := ioutil.ReadAll(req.Body)
+	requestBody := LoginRequest{}
+	util.PrintBody(req)
+	err := json.Unmarshal(bodyBytes, &requestBody)
+	fmt.Printf("%+v", requestBody)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// confirmLogin is up to you to define
+	userID, success := confirmLogin(requestBody)
+	if !success {
+		http.Error(w, "invalid login", http.StatusUnauthorized)
+		return
+	}
+
+	//generate token
+	expireToken := time.Now().Add(time.Hour * 24 * 30).Unix()
+
+	claims := Claims{
+		userID,
+		requestBody.Username,
+		jwt.StandardClaims{
+			ExpiresAt: expireToken,
+			Issuer:    "localhost:8080",
+			Id:        string(userID),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	fmt.Println("Chuan bi");
+	fmt.Println(jwtSecret);
+	signedToken, _ := token.SignedString(jwtSecret)
+
+	//output token
+	tokenResponse := struct {
+		Token  string `json:"token"`
+		UserID uint `json:"userId"`
+	}{signedToken, userID}
+	json.NewEncoder(w).Encode(tokenResponse)
+}
